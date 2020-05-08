51c64d84a907f2a0259e00f045d8634a149820b4
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-05-08 13:42:08 -0400

    Edit app.js

--- app.js
+++ app.js
@@ -4,25 +4,35 @@ const ejs = require("ejs");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"))
 app.set("view engine", "ejs");
 
-
+var items=[];
 app.get("/", function(req, res){
+
 var date = new Date();
-var currentDay = date.getDay();
-var day = "";
+var today = date.getDay();
+day = "";
 
-if (currentDay===6 || currentDay ===0){
-  day = "Weekend"
-}else{
-  day="Weekday"
-}
-res.render("list", {kindOfDay:day})
+var options = {
+  weekday:"long",
+  day:"numeric",
+  month:"long"
+};
 
+var currentDay = date.toLocaleDateString("us-EN", options)
+res.render("list", {kind_of_day:currentDay, items:items})
+
+});
+
+app.post("/", function(req, res){
+  item = req.body.item1
+  items.push(item)
+  res.redirect("/")
 })
 
 
 
 app.listen(3000, function(req, res){
   console.log("Server is running on port 3000");
-})
+});
